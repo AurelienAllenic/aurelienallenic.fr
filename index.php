916c8c5858af9efb318432ba6c92acec5859d674
aurelien <aurelien.allenic@gmail.com>
2021-09-09 17:39:17 +0200

    Version 1

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="author" content="aurelienallenic" />
+        <title>Aurélien Allenic</title>
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <!-- Core theme CSS-->
+        <link href="style.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Aurélien Allenic</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/photo_cv.png" alt="ma photo" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#a_propos">A propos</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Expérience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#formation">formation</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#competences">Compétences</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#centres_interets">Centres d'intérêts</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#lettremotivation">Lettre de motivation</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+
+            <!-- A propos-->
+            <section class="resume-section" id="a_propos">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Aurélien
+                        <span class="text-primary">Allenic</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        <a href="mailto:aurelien.allenic@gmail.com">aurelien.allenic@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">Autodidacte et motivé, je suis à la recherche d'un contrat en alternance suite à mon inscription en année de bachelor à l'<a href="https://etna.io/">ETNA</a>, Ecole des Technologies Numériques Avancées</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!--experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Animateur</h3>
+                            <div class="subheading mb-3">Animations pour la société Dianniversaire</div>
+                            <p>Encadrement de fêtes d'anniversaires pour enfants</p>                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2018-2021</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Stage d'observation</h3>
+                            <div class="subheading mb-3">Atelier du Temps Passé</div>
+                            <p>Stage d'observation dans un atelier de restauration et de conservation d'objets d'oeuvres d'arts</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2015-2016</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Stage d'observation</h3>
+                            <div class="subheading mb-3">Cabinet Ruivo</div>
+                            <p>Stage d'observation au cabinet Ruivo, cabinet spécialisé en droit des assurances</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2013-2014</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!-- formation-->
+            <section class="resume-section" id="formation">
+
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Formation</h2>
+
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Obtention d'une licence</h3>
+                            <div class="subheading mb-3">"Arts du Spectacle : théâtre"</div>
+                            <div>Université Sorbonne-nouvelle - Paris 3</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2020-2021</span></div>
+                    </div>
+
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Première année de licence</h3>
+                            <div class="subheading mb-3">"Arts du Spectacle : théâtre"</div>
+                             <div>Université Sorbonne-nouvelle - Paris 3</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2018-2019</span></div>
+                    </div>
+
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Obtention du Baccalauréat</h3>
+                            <div class="subheading mb-3">Terminale ES - Option Sciences Politiques</div>
+                             <div>Lycée Sainte-Louise 75020</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2017-2018</span></div>
+                    </div>
+
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!-- compétences-->
+            <section class="resume-section" id="competences">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Compétences</h2>
+                    <p>J'ai pu suivre en autodidacte des cours sur <a href="https://openclassrooms.com">openclassroom</a></p>
+                    <div class="subheading mb-3">Cours suivis et en cours d'acquisition :</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            HTML,CSS
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            PHP MySQL
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            JavaScript
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-spinner"></i></span>
+                            Git/GitHub
+                        </li>
+                         <li>
+                            <span class="fa-li"><i class="fas fa-spinner"></i></span>
+                            Bootstrap
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!-- Centre d'intérêts-->
+            <section class="resume-section" id="centres_interets">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Centres d'intérêts</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-code"></i></span>
+                            Programmation informatique
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-music"></i></span>
+                            Piano
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-magic"></i></span>
+                            Prestidigitation
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-fist-raised"></i></span>
+                            Arts martiaux
+                        </li>
+                         <li>
+                            <span class="fa-li"><i class="fas fa-chess-knight"></i></span>
+                            échecs
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!-- lettre de motivation-->
+            <section class="resume-section" id="lettremotivation">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Lettre de motivation</h2>
+                    <p>
+                        Madame, Monsieur,<br><br>
+
+Je suis actuellement en recherche d'un contrat d'alternance.<br>
+Suite à l’obtention d’une licence d’arts du spectacle, j’ai choisi de changer de voie pour m’orienter vers le domaine du développement informatique en m’inscrivant en année de Bachelor à l'ETNA. <br>
+C’est notamment grâce à des proches, mais aussi appuyé par des sites internet tels qu’Open Classroom que j’ai pu étudier en quasi-autodidacte les technologies HTML, CSS, PHP, MySQL, Git/GitHub, ou encore JavaScript plus récemment.<br>
+En tant qu’autodidacte et futur étudiant en informatique, c’est tout mon sérieux et ma détermination que je mettrais au service des tâches qui me seront confiées. L’alternance consiste en une journée par mois de cours et le reste du temps en entreprise. Par ailleurs, l’ETNA organise en début d’année une « piscine » qui, pendant six semaines intensives, me donnera la possibilité d’acquérir et de consolider des bases importantes et de valider l’expérience acquise au cours de cette période. <br>
+Des objectifs de réalisation de projets pour l’école, effectués en entreprise et nommées « run » viendront mobiliser 23 heures toutes les trois semaines. Ces heures sont généralement placées le mardi et jeudi mais sont modifiables à la demande de l’entreprise.<br>
+Etant en cours d’apprentissage, je suis ouvert, intéressé par tout ce qui pourrait me donner des acquisitions solides autant théoriques que pratiques. Ma compétence actuelle est tournée vers le développement web, ce qui m’a d’ores et déjà amené à la réalisation de projets personnels de création de sites incluant des bases de données. Qu’il s’agisse de renforcer ces connaissances ou bien de m’amener vers d’autres, je suis motivé et prêt à assumer tous les efforts qui me conduiront pas à pas vers la professionnalisation.<br>
+Je souhaite intégrer une entreprise pour apprendre en situation réelle, me donner la possibilité de faire montre de toute l’énergie, du sérieux et de l’implication dont je suis capable.
+Je me tiens à votre entière disposition en vue d’entretiens ou tests préalables à une embauche en alternance de la part de votre entreprise.<br><br>
+Dans cette attente, <br><br>
+
+Je vous prie d’agréer, Monsieur, Madame, l’expression de mes salutations distinguées
+</p>
+
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="scripts.js"></script>
+    </body>
+</html>
